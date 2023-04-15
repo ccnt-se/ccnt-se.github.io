0e77759f049fb2eddc31cde915afe156b4ecd851
wtx <535705541@qq.com>
2023-04-15 15:46:24 +0800

    fix: image display

--- src/pages/ToolList/index.tsx
+++ src/pages/ToolList/index.tsx
@@ -1,50 +1,55 @@
-import { PageContainer } from '@ant-design/pro-components';
-import React, { useEffect, useState } from 'react';
+import {PageContainer} from '@ant-design/pro-components';
+import React, {useEffect, useState} from 'react';
 import styles from './styles.less';
-import { history } from 'umi';
-import { createFromIconfontCN, GithubFilled, PlayCircleFilled } from '@ant-design/icons';
-import { Carousel, Col, Row, Tooltip, Typography } from 'antd';
-import { useIntl } from '@umijs/max';
+import './styles.less';
+import {history} from 'umi';
+import {createFromIconfontCN, GithubFilled, PlayCircleFilled} from '@ant-design/icons';
+import {Carousel, Col, Image, Row, Tooltip, Typography} from 'antd';
+import {useIntl} from '@umijs/max';
 import '@/services/tool/typings.d.ts';
 
-const { Paragraph } = Typography;
+const {Paragraph} = Typography;
 
 const IconFont = createFromIconfontCN({
   scriptUrl: '//at.alicdn.com/t/c/font_3985552_xhu21628atl.js',
 });
 
-const ListItem: React.FC<{ info: API.Tool.Info }> = ({ info }) => {
-  const { formatMessage, locale } = useIntl();
+const ListItem: React.FC<{ info: API.Tool.Info }> = ({info}) => {
+  const {formatMessage, locale} = useIntl();
 
   const iconSize = 32;
   const icons: { [key: string]: API.Tool.TagIcon } = {
     github: {
-      tooltip: formatMessage({ id: 'pages.toolList.githubIcon' }),
-      icon: <GithubFilled style={{ fontSize: `${iconSize}px`, color: '#000' }} />,
+      tooltip: formatMessage({id: 'pages.toolList.githubIcon'}),
+      icon: <GithubFilled style={{fontSize: `${iconSize}px`, color: '#000'}}/>,
     },
     video: {
-      tooltip: formatMessage({ id: 'pages.toolList.videoIcon' }),
-      icon: <PlayCircleFilled style={{ fontSize: `${iconSize}px`, color: '#c50000' }} />,
+      tooltip: formatMessage({id: 'pages.toolList.videoIcon'}),
+      icon: <PlayCircleFilled style={{fontSize: `${iconSize}px`, color: '#c50000'}}/>,
     },
     docker: {
-      tooltip: formatMessage({ id: 'pages.toolList.dockerIcon' }),
-      icon: <IconFont type="icon-docker" style={{ fontSize: `${iconSize}px`, color: '#0db7ed' }} />,
+      tooltip: formatMessage({id: 'pages.toolList.dockerIcon'}),
+      icon: <IconFont type="icon-docker" style={{fontSize: `${iconSize}px`, color: '#0db7ed'}}/>,
     },
     ideaPlugin: {
-      tooltip: formatMessage({ id: 'pages.toolList.ideaPluginIcon' }),
-      icon: <IconFont type="icon-Idea" style={{ fontSize: `${iconSize}px` }} />,
+      tooltip: formatMessage({id: 'pages.toolList.ideaPluginIcon'}),
+      icon: <IconFont type="icon-Idea" style={{fontSize: `${iconSize}px`}}/>,
     },
     compiledProduct: {
-      tooltip: formatMessage({ id: 'pages.toolList.compiledProduct' }),
+      tooltip: formatMessage({id: 'pages.toolList.compiledProduct'}),
       icon: (
         <IconFont
           type="icon-product-checked-fill"
-          style={{ fontSize: `${iconSize}px`, color: '#008600' }}
+          style={{fontSize: `${iconSize}px`, color: '#008600'}}
         />
       ),
     },
   };
 
+  /**
+   * 获取介绍, 优先使用当前语言的介绍
+   * @param introduction
+   */
   const getIntroduction = (introduction: API.Tool.Info['introduction']): string => {
     console.log(typeof introduction);
     if (typeof introduction === 'string') {
@@ -68,7 +73,7 @@ const ListItem: React.FC<{ info: API.Tool.Info }> = ({ info }) => {
     <div className={styles.listItem} onClick={() => history.push(`/tool/${info.name}`)}>
       <Row>
         {/* 内容区域 */}
-        <Col lg={16} md={14} sm={12} xs={24}>
+        <Col lg={16} md={14} sm={24} xs={24} style={{padding: '20px 20px 20px 40px', maxHeight: 240}}>
           <div className={styles.listItemTitle}>{info.name}</div>
           <div className={styles.listItemIconGroup}>
             {info.tagIcons.map((key: string) => {
@@ -80,22 +85,30 @@ const ListItem: React.FC<{ info: API.Tool.Info }> = ({ info }) => {
               );
             })}
           </div>
-          <Paragraph ellipsis={{ rows: 3, expandable: false }}>
+          <Paragraph className={styles.listItemIntroduction} ellipsis={{rows: 3, expandable: false}}>
             {getIntroduction(info.introduction)}
           </Paragraph>
         </Col>
 
         {/* 图片区域 */}
-        <Col lg={8} md={10} sm={12} xs={24} style={{ backgroundColor: '#ddd' }}>
-          <Carousel style={{ height: '100%', backgroundColor: '#0dd' }}></Carousel>
+        <Col lg={8} md={10} sm={24} xs={24} onClick={(event) => event.stopPropagation()}>
+          <Carousel>
+            <div>
+              <Image src="https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg"/>
+            </div>
+            <div>
+              <Image src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"/>
+            </div>
+          </Carousel>
         </Col>
       </Row>
     </div>
   );
 };
 
+
 const ToolList: React.FC = () => {
-  const [data, setData] = useState<API.Tool.JSONData>({ tools: [] });
+  const [data, setData] = useState<API.Tool.JSONData>({tools: []});
 
   useEffect(() => {
     fetch('/data/tools.json')
@@ -106,7 +119,7 @@ const ToolList: React.FC = () => {
   return (
     <PageContainer title={false}>
       {data.tools.map((item: any) => {
-        return <ListItem key={item.name} info={item} />;
+        return <ListItem key={item.name} info={item}/>;
       })}
     </PageContainer>
   );
